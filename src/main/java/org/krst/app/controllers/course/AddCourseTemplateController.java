16b51cbe81a9f84fad2b4367367d9f1d84bb9452
YICHEN ZHAO <yzhao179@ucsc.edu>
2020-12-28 19:30:51 -0800

    fix AddCourseTemplateController.java half bug and test for listener

--- src/main/java/org/krst/app/controllers/course/AddCourseTemplateController.java
+++ src/main/java/org/krst/app/controllers/course/AddCourseTemplateController.java
@@ -1,6 +1,8 @@
 package org.krst.app.controllers.course;
 
+import com.sun.xml.bind.v2.runtime.unmarshaller.XsiNilLoader;
 import de.felixroske.jfxsupport.FXMLController;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
@@ -9,13 +11,15 @@ import org.krst.app.KRSTManagementSoftware;
 import org.krst.app.domains.CourseTemplate;
 import org.krst.app.domains.Teacher;
 import org.krst.app.repositories.CourseTemplateRepository;
-import org.krst.app.repositories.Logger;
 import org.krst.app.repositories.TeacherRepository;
 import org.krst.app.services.CacheService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.swing.*;
 import java.net.URL;
+import java.util.Comparator;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 @FXMLController
@@ -31,6 +35,8 @@ public class AddCourseTemplateController implements Initializable {
     private ComboBox<Teacher> teacherSelector;
     @FXML
     private ListView<Teacher> teachers;
+    @FXML
+    private ListView<Teacher> emptyteachers;
 
     @Autowired
     private TeacherRepository teacherRepository;
@@ -43,6 +49,91 @@ public class AddCourseTemplateController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         teacherSelector.getItems().addAll(teacherRepository.findAll());
 
+        flushonetime();
+
+
+        teacherSelector.getSelectionModel().selectedIndexProperty().addListener((observable, oldValue, newValue) -> {
+            //Teacher selectedItem=teacherSelector.getSelectionModel().getSelectedItem();
+            // teacherSelector.getSelectionModel().clearSelection();
+            System.out.println("newvalue:"+newValue);
+            System.out.println("oldvalue:"+oldValue);
+            System.out.println("observe:"+observable);
+            System.out.println(Thread.currentThread().getId());
+            int bigestindex=0;
+            if((int)newValue>bigestindex)
+            {bigestindex=(int)newValue;}
+            Teacher selectedItem=teacherSelector.getItems().get(bigestindex);
+            System.out.println("saveindex="+bigestindex);
+            //System.out.println("index: "+teacherSelector.getSelectionModel().getSelectedIndex());
+            if (selectedItem!= null) {
+
+                //System.out.println("1"+teacherSelector.getSelectionModel().getSelectedItem());
+
+                addTeacherToTeachers(selectedItem);
+
+                teacherSelector.getItems().remove(selectedItem);
+                //teacherSelector.getSelectionModel().clearSelection(teacherSelector.getSelectionModel().getSelectedIndex());
+                teacherSelector.getSelectionModel().clearSelection();
+
+                System.out.println("2"+teacherSelector.getSelectionModel().getSelectedItem());
+                flushonetime();
+
+            }
+            System.out.println("reach end");
+
+        });
+        teachers.setOnMouseClicked(event -> {
+            if (event.getClickCount() == 2) {
+                Teacher selectedItem=teachers.getSelectionModel().getSelectedItem();
+                if(selectedItem!=null) {
+                    teacherSelector.getItems().add(selectedItem);
+                    teachers.getItems().remove(teachers.getSelectionModel().getSelectedIndex());
+                    flushonetime();
+                }
+            }
+        });
+        flushonetime();
+    }
+//    teacherSelector.setOnMouseClicked(event -> {
+//        if (event.getClickCount() == 1) {
+//            Teacher selectedItem=teacherSelector.getSelectionModel().getSelectedItem();
+//            if (selectedItem != null) {
+//                //System.out.println(selectedItem);
+//                addTeacherToTeachers(selectedItem);
+//
+//                teacherSelector.getSelectionModel().clearSelection();
+//
+//                //    oldValue=newValue;
+//            }}
+//
+//    });
+    private void addTeacherToTeachers(Teacher teacher) {
+        if (!teachers.getItems().contains(teacher)) {
+            teachers.getItems().add(teacher);
+            teachers.getItems().stream().sorted().distinct();
+
+        //    teacherSelector.getItems().addAll(teacherRepository.findAll());
+        }
+    }
+    private  void flushonetime(){
+        teachers.getItems().stream().sorted().distinct();
+        teacherSelector.getItems().stream().sorted().distinct();
+        teachers.setCellFactory(new Callback<ListView<Teacher>, ListCell<Teacher>>() {
+            @Override
+            public ListCell<Teacher> call(ListView<Teacher> param) {
+                return new ListCell<Teacher>() {
+                    @Override
+                    protected void updateItem(Teacher item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (item == null || empty) {
+                            setGraphic(null);
+                        } else {
+                            setText(item.getName() + " [" + item.getId() + "]");
+                        }
+                    }
+                };
+            }
+        });
         teacherSelector.setCellFactory(new Callback<ListView<Teacher>, ListCell<Teacher>>() {
             @Override
             public ListCell<Teacher> call(ListView<Teacher> param) {
@@ -60,42 +151,7 @@ public class AddCourseTemplateController implements Initializable {
             }
         });
 
-        teacherSelector.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue != null) {
-                addTeacherToTeachers(newValue);
-            }
-            teacherSelector.getSelectionModel().clearSelection();
-        });
 
-        teachers.setCache(false);
-        teachers.setCellFactory(new Callback<ListView<Teacher>, ListCell<Teacher>>() {
-            @Override
-            public ListCell<Teacher> call(ListView<Teacher> param) {
-                return new ListCell<Teacher>() {
-                    @Override
-                    protected void updateItem(Teacher item, boolean empty) {
-                        super.updateItem(item, empty);
-                        if (item == null || empty) {
-                            setGraphic(null);
-                        } else {
-                            setText(item.getName() + " [" + item.getId() + "]");
-                        }
-                    }
-                };
-            }
-        });
-
-        teachers.setOnMouseClicked(event -> {
-            if (event.getClickCount() == 2) {
-                teachers.getItems().remove(teachers.getSelectionModel().getSelectedIndex());
-            }
-        });
-    }
-
-    private void addTeacherToTeachers(Teacher teacher) {
-        if (!teachers.getItems().contains(teacher)) {
-            teachers.getItems().add(teacher);
-        }
     }
 
     public void approve() {
@@ -113,7 +169,6 @@ public class AddCourseTemplateController implements Initializable {
             alert.showAndWait();
         } else {
             courseTemplateRepository.save(courseTemplate);
-            Logger.logInfo(getClass().toString(), "新建课程模板，编号：{}，名称：{}", courseTemplate.getId(), courseTemplate.getName());
             cacheService.refreshCourseTemplateCache();
             KRSTManagementSoftware.closeWindow();
         }
