4b763e6b5efb247aa4f3e09e6d08bd860bf84b1b
Hang Yuan <hyuan3@ucsc.edu>
2021-01-01 12:38:58 -0800

    KRST-12, Enhancement, Revise AddStaff w/ Controller (#15)

--- src/main/java/org/krst/app/controllers/staff/AddStaffController.java
+++ src/main/java/org/krst/app/controllers/staff/AddStaffController.java
@@ -3,7 +3,7 @@ package org.krst.app.controllers.staff;
 import de.felixroske.jfxsupport.FXMLController;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import org.krst.app.KRSTManagementSoftware;
+import javafx.stage.Stage;
 import org.krst.app.domains.Staff;
 import org.krst.app.configurations.Logger;
 import org.krst.app.repositories.StaffRepository;
@@ -65,7 +65,16 @@ public class AddStaffController {
             alert.setHeaderText("失败原因：未填入员工编号");
             alert.setContentText("解决方法：请输入员工编号");
             alert.showAndWait();
+            return;
+        } else if (staffRepository.existsById(id.getText())) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("新建员工档案错误");
+            alert.setHeaderText("错误原因：使用已存在的员工编号");
+            alert.setContentText("解决方法：请输入不同的员工编号");
+            alert.showAndWait();
+            return;
         }
+
         staff.setName(name.getText());
         staff.setBaptismalName(baptismalName.getText());
         staff.setGender(gender_male.isSelected() ? "男" : "女");
@@ -85,20 +94,12 @@ public class AddStaffController {
         staff.setTalent(talent.getText());
         staff.setEducation(education.getText());
 
-        if (staffRepository.existsById(staff.getId())) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("新建员工档案错误");
-            alert.setHeaderText("错误原因：使用已存在的员工编号");
-            alert.setContentText("解决方法：请输入不同的员工编号");
-            alert.showAndWait();
-        } else {
-            staffRepository.save(staff);
-            logger.logInfo(getClass().toString(), "新建员工档案，编号：{}，姓名：{}", id.getText(), name.getText());
-            KRSTManagementSoftware.closeWindow();
-        }
+        staffRepository.save(staff);
+        logger.logInfo(getClass().toString(), "新建员工档案，编号：{}，姓名：{}", id.getText(), name.getText());
+        close();
     }
 
-    public void cancel() {
-        KRSTManagementSoftware.closeWindow();
+    public void close() {
+        ((Stage)id.getScene().getWindow()).close();
     }
 }
