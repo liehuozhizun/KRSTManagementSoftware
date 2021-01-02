1a3ebb3bf41f94dd69440699479e6d211fc4db3e
Hang Yuan <hyuan3@ucsc.edu>
2021-01-02 10:55:22 -0800

    KRST-13, Enhancement, Revise AddStudent w/ controller (#16)

--- src/main/java/org/krst/app/views/student/AddStudent.java
+++ src/main/java/org/krst/app/views/student/AddStudent.java
@@ -2,8 +2,10 @@ package org.krst.app.views.student;
 
 import de.felixroske.jfxsupport.AbstractFxmlView;
 import de.felixroske.jfxsupport.FXMLView;
+import org.springframework.context.annotation.Scope;
 
 @FXMLView(title = "新建学生档案")
+@Scope("prototype")
 public class AddStudent extends AbstractFxmlView {
 
 }
