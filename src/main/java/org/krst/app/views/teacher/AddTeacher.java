643350fd822025b67ae0f0f9a28d39d283b38374
liehuozhizun <hyuan3@ucsc.edu>
2021-01-02 11:24:30 -0800

    KRST-15, Enhancement, Revise AddTeacher w/ controller

--- src/main/java/org/krst/app/views/teacher/AddTeacher.java
+++ src/main/java/org/krst/app/views/teacher/AddTeacher.java
@@ -2,7 +2,9 @@ package org.krst.app.views.teacher;
 
 import de.felixroske.jfxsupport.AbstractFxmlView;
 import de.felixroske.jfxsupport.FXMLView;
+import org.springframework.context.annotation.Scope;
 
 @FXMLView(title = "新建教师档案")
+@Scope("prototype")
 public class AddTeacher extends AbstractFxmlView {
 }
