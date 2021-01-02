75311c4e9d4473e4588d547aff3320a237281f09
liehuozhizun <hyuan3@ucsc.edu>
2021-01-02 11:18:34 -0800

    KRST-6, Enhancement, Revise AddCourseTemplate w/ controller

--- src/main/java/org/krst/app/controllers/course/AddCourseTemplateController.java
+++ src/main/java/org/krst/app/controllers/course/AddCourseTemplateController.java
@@ -3,9 +3,8 @@ package org.krst.app.controllers.course;
 import de.felixroske.jfxsupport.FXMLController;
 
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import org.krst.app.KRSTManagementSoftware;
+import javafx.stage.Stage;
 import org.krst.app.controllers.ControllerTemplate;
 import org.krst.app.domains.CourseTemplate;
 import org.krst.app.domains.Teacher;
@@ -14,12 +13,10 @@ import org.krst.app.repositories.TeacherRepository;
 import org.krst.app.services.CacheService;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.net.URL;
 import java.util.HashSet;
-import java.util.ResourceBundle;
 
 @FXMLController
-public class AddCourseTemplateController extends ControllerTemplate implements Initializable {
+public class AddCourseTemplateController extends ControllerTemplate {
 
     @FXML
     private TextField id;
@@ -39,8 +36,8 @@ public class AddCourseTemplateController extends ControllerTemplate implements I
     @Autowired
     private CacheService cacheService;
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    @FXML
+    public void initialize() {
         teacherSelector.getItems().addAll(teacherRepository.findAll());
         setUpSelectorAndList(teacherSelector, teachers);
     }
@@ -52,26 +49,27 @@ public class AddCourseTemplateController extends ControllerTemplate implements I
     }
 
     public void approve() {
+        if (courseTemplateRepository.existsById(id.getText())) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("新建课程模板失败");
+            alert.setHeaderText("失败原因：课程编号已存在");
+            alert.setContentText("解决方法：更换课程编号");
+            alert.showAndWait();
+            return;
+        }
+
         CourseTemplate courseTemplate = new CourseTemplate(
                 id.getText(),
                 name.getText(),
                 topic.getText(),
                 new HashSet<>(teachers.getItems()));
 
-        if (courseTemplateRepository.existsById(courseTemplate.getId())) {
-            Alert alert = new Alert(Alert.AlertType.WARNING);
-            alert.setTitle("新建课程模板失败");
-            alert.setHeaderText("失败原因：课程编号已存在");
-            alert.setContentText("解决方法：更换课程编号");
-            alert.showAndWait();
-        } else {
-            courseTemplateRepository.save(courseTemplate);
-            cacheService.refreshCourseTemplateCache();
-            KRSTManagementSoftware.closeWindow();
-        }
+        courseTemplateRepository.save(courseTemplate);
+        cacheService.refreshCourseTemplateCache();
+        close();
     }
 
-    public void cancel() {
-        KRSTManagementSoftware.closeWindow();
+    public void close() {
+        ((Stage)id.getScene().getWindow()).close();
     }
 }
