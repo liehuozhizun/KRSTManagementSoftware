a9ddd1632585e1fa226d09707ae04634e78dc544
yzhao179 <76461566+yzhao179@users.noreply.github.com>
2021-01-01 19:56:55 -0800

    KRST-1, Fix, Logics between ComboBox and ListView (#8)
    * fix AddCourseTemplateController.java half bug and test for listener
    
    * fix ALL problem
    
    * Abstract the common logics that are needed for data exchange between selector and listview
    
    Co-authored-by: liehuozhizun <hyuan3@ucsc.edu>

--- src/main/java/org/krst/app/controllers/course/AddCourseTemplateController.java
+++ src/main/java/org/krst/app/controllers/course/AddCourseTemplateController.java
@@ -1,12 +1,12 @@
 package org.krst.app.controllers.course;
 
 import de.felixroske.jfxsupport.FXMLController;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import javafx.util.Callback;
 import org.krst.app.KRSTManagementSoftware;
-import org.krst.app.configurations.Logger;
+import org.krst.app.controllers.ControllerTemplate;
 import org.krst.app.domains.CourseTemplate;
 import org.krst.app.domains.Teacher;
 import org.krst.app.repositories.CourseTemplateRepository;
@@ -19,7 +19,7 @@ import java.util.HashSet;
 import java.util.ResourceBundle;
 
 @FXMLController
-public class AddCourseTemplateController implements Initializable {
+public class AddCourseTemplateController extends ControllerTemplate implements Initializable {
 
     @FXML
     private TextField id;
@@ -38,66 +38,17 @@ public class AddCourseTemplateController implements Initializable {
     private CourseTemplateRepository courseTemplateRepository;
     @Autowired
     private CacheService cacheService;
-    @Autowired
-    private Logger logger;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         teacherSelector.getItems().addAll(teacherRepository.findAll());
-
-        teacherSelector.setCellFactory(new Callback<ListView<Teacher>, ListCell<Teacher>>() {
-            @Override
-            public ListCell<Teacher> call(ListView<Teacher> param) {
-                return new ListCell<Teacher>() {
-                    @Override
-                    protected void updateItem(Teacher item, boolean empty) {
-                        super.updateItem(item, empty);
-                        if (item == null || empty) {
-                            setGraphic(null);
-                        } else {
-                            setText(item.getName() + " [" + item.getId() + "]");
-                        }
-                    }
-                };
-            }
-        });
-
-        teacherSelector.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue != null) {
-                addTeacherToTeachers(newValue);
-            }
-            teacherSelector.getSelectionModel().clearSelection();
-        });
-
-        teachers.setCache(false);
-        teachers.setCellFactory(new Callback<ListView<Teacher>, ListCell<Teacher>>() {
-            @Override
-            public ListCell<Teacher> call(ListView<Teacher> param) {
-                return new ListCell<Teacher>() {
-                    @Override
-                    protected void updateItem(Teacher item, boolean empty) {
-                        super.updateItem(item, empty);
-                        if (item == null || empty) {
-                            setGraphic(null);
-                        } else {
-                            setText(item.getName() + " [" + item.getId() + "]");
-                        }
-                    }
-                };
-            }
-        });
-
-        teachers.setOnMouseClicked(event -> {
-            if (event.getClickCount() == 2) {
-                teachers.getItems().remove(teachers.getSelectionModel().getSelectedIndex());
-            }
-        });
+        setUpSelectorAndList(teacherSelector, teachers);
     }
 
-    private void addTeacherToTeachers(Teacher teacher) {
-        if (!teachers.getItems().contains(teacher)) {
-            teachers.getItems().add(teacher);
-        }
+    @Override
+    public void flush(){
+        teachers.setCellFactory(getCellFactory());
+        teacherSelector.setCellFactory(getCellFactory());
     }
 
     public void approve() {
@@ -115,7 +66,6 @@ public class AddCourseTemplateController implements Initializable {
             alert.showAndWait();
         } else {
             courseTemplateRepository.save(courseTemplate);
-            logger.logInfo(getClass().toString(), "新建课程模板，编号：{}，名称：{}", courseTemplate.getId(), courseTemplate.getName());
             cacheService.refreshCourseTemplateCache();
             KRSTManagementSoftware.closeWindow();
         }
