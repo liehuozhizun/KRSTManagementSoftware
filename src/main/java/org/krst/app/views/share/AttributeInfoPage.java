43f451308af74fcdf8300a5283ca65b08bd81438
Hang Yuan <hyuan3@ucsc.edu>
2021-01-11 22:26:40 -0800

    KRST-33, Feature, Add AttributeInfoPage w/ Controller (#26)

---
+++ src/main/java/org/krst/app/views/share/AttributeInfoPage.java
@@ -0,0 +1,10 @@
+package org.krst.app.views.share;
+
+import de.felixroske.jfxsupport.AbstractFxmlView;
+import de.felixroske.jfxsupport.FXMLView;
+import org.springframework.context.annotation.Scope;
+
+@FXMLView(title = "所属教区记录")
+@Scope("prototype")
+public class AttributeInfoPage extends AbstractFxmlView {
+}
