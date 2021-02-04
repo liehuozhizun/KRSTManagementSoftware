9eb1c21d96afbd5244e5039af23c44ba09b86bf2
Hang Yuan <hyuan3@ucsc.edu>
2021-02-04 12:00:26 -0800

    KRST-43, Feature, Add PersonInfoPage w/ Controller (#38)

--- src/main/java/org/krst/app/views/teacher/TeacherInfoPage.java
+++ src/main/java/org/krst/app/views/teacher/TeacherInfoPage.java
@@ -5,6 +5,6 @@ import de.felixroske.jfxsupport.FXMLView;
 import org.springframework.context.annotation.Scope;
 
 @Scope("prototype")
-@FXMLView(title = "教师档案查询")
+@FXMLView(title = "教师档案")
 public class TeacherInfoPage extends AbstractFxmlView {
 }
