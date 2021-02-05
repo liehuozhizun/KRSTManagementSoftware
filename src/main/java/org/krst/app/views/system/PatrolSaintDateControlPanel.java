2d645a68d610b0d147edab9ab46811d575d814d0
Hang Yuan <hyuan3@ucsc.edu>
2021-02-04 21:24:41 -0800

    KRST-45, Feature, Add PatronSaintDateControlPanel w/ Controller (#40)

---
+++ src/main/java/org/krst/app/views/system/PatrolSaintDateControlPanel.java
@@ -0,0 +1,8 @@
+package org.krst.app.views.system;
+
+import de.felixroske.jfxsupport.AbstractFxmlView;
+import de.felixroske.jfxsupport.FXMLView;
+
+@FXMLView(title = "圣人纪念日控制面板")
+public class PatrolSaintDateControlPanel extends AbstractFxmlView {
+}
