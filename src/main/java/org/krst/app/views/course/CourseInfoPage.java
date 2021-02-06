4030aabcb463ead6abba621979b9ff86ce702a70
Hang Yuan <hyuan3@ucsc.edu>
2021-02-05 17:55:49 -0800

    KRST-22, Feature, Add CourseInfoPage w/ Controller (#44)

---
+++ src/main/java/org/krst/app/views/course/CourseInfoPage.java
@@ -0,0 +1,10 @@
+package org.krst.app.views.course;
+
+import de.felixroske.jfxsupport.AbstractFxmlView;
+import de.felixroske.jfxsupport.FXMLView;
+import org.springframework.context.annotation.Scope;
+
+@FXMLView(title = "课程信息详情")
+@Scope("prototype")
+public class CourseInfoPage extends AbstractFxmlView {
+}
