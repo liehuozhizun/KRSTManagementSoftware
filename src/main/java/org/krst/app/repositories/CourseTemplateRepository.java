1bfa9727baba67d78cd3d9d288a6ccc76c67e753
Hang Yuan <hyuan3@ucsc.edu>
2021-03-20 19:38:21 -0700

    KRST-59, Fix, Fix Course_id & CourseTemplate_id mapping (#58)

--- src/main/java/org/krst/app/repositories/CourseTemplateRepository.java
+++ src/main/java/org/krst/app/repositories/CourseTemplateRepository.java
@@ -2,8 +2,16 @@ package org.krst.app.repositories;
 
 import org.krst.app.domains.CourseTemplate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 @Repository
+@Transactional
 public interface CourseTemplateRepository extends JpaRepository<CourseTemplate, String> {
+    @Modifying
+    @Query(value = "UPDATE course SET course_template_id = :newId WHERE course_template_id = :oldId", nativeQuery = true)
+    void updateTemplateIdInCourse(String oldId, String newId);
 }
