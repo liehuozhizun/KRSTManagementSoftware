8c68559453f823fc6796ac12627ea0ecc306a7c4
Hang Yuan <hyuan3@ucsc.edu>
2021-03-21 22:34:22 -0700

    KRST-62, Fix, Fix Relationship displays wrong data issue (#62)

--- src/main/java/org/krst/app/controllers/person/PersonInfoPageController.java
+++ src/main/java/org/krst/app/controllers/person/PersonInfoPageController.java
@@ -12,11 +12,13 @@ import org.krst.app.configurations.Logger;
 import org.krst.app.controllers.InfoPageControllerTemplate;
 import org.krst.app.domains.Person;
 import org.krst.app.domains.Relation;
+import org.krst.app.models.RelationPassModel;
 import org.krst.app.repositories.PersonRepository;
 import org.krst.app.services.DataPassService;
 import org.krst.app.services.RelationshipService;
 import org.krst.app.utils.CommonUtils;
 import org.krst.app.views.share.AddRelationship;
+import org.krst.app.views.share.RelationshipInfoPage;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /*
@@ -101,6 +103,39 @@ public class PersonInfoPageController implements InfoPageControllerTemplate {
                     }
                 };
             }
+        });
+        relationship.setRowFactory( tv -> {
+            TableRow<Relation> row = new TableRow<>();
+            row.setOnMouseClicked(event -> {
+                if (event.getClickCount() == 2 && (!row.isEmpty()) ) {
+                    Relation tempRelation = row.getItem();
+                    RelationPassModel relationPassModel = new RelationPassModel(
+                            Relation.Type.PERSON,
+                            originalPerson.getId(),
+                            originalPerson.getName(),
+                            tempRelation.getType(),
+                            tempRelation.getId(),
+                            tempRelation.getName(),
+                            tempRelation.getRelationship()
+                    );
+                    dataPassService.setValue(relationPassModel);
+                    KRSTManagementSoftware.openWindow(RelationshipInfoPage.class);
+                    Pair<Boolean, String> returnedData = (Pair<Boolean, String>) dataPassService.getValue();
+                    if (returnedData != null) {
+                        if (returnedData.getKey()) {
+                            originalPerson.getRelationships().remove(row.getItem());
+                            Relation temp = row.getItem();
+                            temp.setRelationship(returnedData.getValue());
+                            relationship.getItems().set(row.getIndex(), temp);
+                            originalPerson.getRelationships().add(row.getItem());
+                        } else {
+                            originalPerson.getRelationships().remove(row.getItem());
+                            relationship.getItems().remove(row.getItem());
+                        }
+                    }
+                }
+            });
+            return row ;
         });
     }
 
